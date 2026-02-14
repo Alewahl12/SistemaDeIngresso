a9f18c26cac203842dab0922741cf47203325486
Alexandre Wahl <alexandre.wahl12@gmail.com>
2026-02-14 02:51:23 -0300

    feat: validacao adicionada e otimizacao da logica de compra

--- js/app.js
+++ js/app.js
@@ -1,33 +1,39 @@
 function comprar(){
     //Pegar tipo do ingresso
     let tipoIngresso = document.getElementById('tipo-ingresso').value;
-    tipoIngresso = `qtd-${tipoIngresso}`;
-    console.log(tipoIngresso);
+
     //Pegar a quantidade de ingressos
     let quantidadeIngresso = parseInt(document.getElementById('qtd').value);
-    console.log(quantidadeIngresso);
+
+    //Pegar campo de quantidade para zerar o input anterior
+    let quantidadeIngressoTexto = document.getElementById('qtd');
+    
+    if (quantidadeIngresso <= 0 || isNaN(quantidadeIngresso)){
+        alert('Quantidade inválida!');
+        quantidadeIngressoTexto.value = '';
+        return;
+    }
 
     //Descontar da quantidade disponivel a quantidade do ingresso selecionado
-    let selecionarIngressosDisponivel = document.getElementById(tipoIngresso);
-    switch (tipoIngresso){
-        case "qtd-pista":
-            quantidadePista -= quantidadeIngresso;
-            selecionarIngressosDisponivel.innerHTML = quantidadePista;
-            break;
-        case "qtd-superior":
-            quantidadeSuperior -= quantidadeIngresso;
-            selecionarIngressosDisponivel.innerHTML = quantidadeSuperior;
-            break;
-        case "qtd-inferior":
-            quantidadeInferior -= quantidadeIngresso;
-            selecionarIngressosDisponivel.innerHTML = quantidadeInferior;
-            break;
-        default:
-            console.log('Tipo de ingresso invalido!');
-    }
+    verificarQuantidadeDisponivel(tipoIngresso,quantidadeIngresso);
+    quantidadeIngressoTexto.value = '';
 
 }
 
-let quantidadePista = parseInt(document.getElementById('qtd-pista').textContent);
-let quantidadeSuperior = parseInt(document.getElementById('qtd-superior').textContent);
-let quantidadeInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+//Funcao para verificar a quantidade de ingressos disponivel e realizar a compra
+function verificarQuantidadeDisponivel(tipo,quantidadeDesejada){
+    let selecionarIngressosDisponivel = document.getElementById(`qtd-${tipo}`);
+    if (estoqueIngressos[tipo]>=quantidadeDesejada){
+        estoqueIngressos[tipo] -= quantidadeDesejada;
+        selecionarIngressosDisponivel.innerHTML = estoqueIngressos[tipo];
+    }else{
+        alert('Quantidade desejada não pode ser maior que a disponivel');
+    }
+}
+
+//Dicionario dos tipos de ingressos
+let estoqueIngressos = {
+    pista: parseInt(document.getElementById('qtd-pista').textContent),
+    superior: parseInt(document.getElementById('qtd-superior').textContent),
+    inferior: parseInt(document.getElementById('qtd-inferior').textContent)
+}
